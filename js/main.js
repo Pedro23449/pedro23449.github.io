202d9c4ec344cf606006cd1fb37aa1f5dde0d460
Pedro23449 <74308061+Pedro23449@users.noreply.github.com>
2023-05-19 21:47:00 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-// Ocultar todos los submenús al cargar la página
-$(document).ready(function() {
-  $(".submenu").hide();
-
-  // Mostrar u ocultar los submenús según el enlace clicado
-  $(".dropdown-link").click(function(e) {
-    e.preventDefault();
-    var submenu = $(this).next(".submenu");
-    var submenuId = submenu.attr("id");
-
-    if (submenu.is(":visible")) {
-      submenu.hide();
-      saveSubmenuState(submenuId, false); // Guardar el estado del submenú como cerrado
-    } else {
-      submenu.show();
-      saveSubmenuState(submenuId, true); // Guardar el estado del submenú como abierto
-    }
-  });
-
-  loadSubmenuState("submenu-guia"); // Cargar el estado del submenú de "Guía zona por zona"
-  loadSubmenuState("submenu-objetos"); // Cargar el estado del submenú de "Guía de Objetos"
-  loadSubmenuState("submenu-misiones"); // Cargar el estado del submenú de "Lista de Misiones"
-  loadSubmenuState("submenu-otros"); // Cargar el estado del submenú de "Otros"
-});
-
 // Función para guardar el estado del submenú en el almacenamiento local
 function saveSubmenuState(submenuId, isOpen) {
   localStorage.setItem(submenuId, isOpen ? "open" : "closed");
@@ -39,4 +14,29 @@ function loadSubmenuState(submenuId) {
     submenu.hide();
   }
 }
+
+// Ocultar todos los submenús al cargar la página
+$(document).ready(function() {
+  $(".submenu").hide();
+  loadSubmenuState("submenu-guia"); // Cargar el estado del submenú de "Guía zona por zona"
+  loadSubmenuState("submenu-objetos"); // Cargar el estado del submenú de "Guía de Objetos"
+  loadSubmenuState("submenu-misiones"); // Cargar el estado del submenú de "Lista de Misiones"
+  loadSubmenuState("submenu-otros"); // Cargar el estado del submenú de "Otros"
+  loadSubmenuState("submenu-rocket"); // Cargar el estado del submenú de "Empresa Rocket"
+
+  // Mostrar u ocultar los submenús según el enlace clicado
+  $(".dropdown-link").click(function(e) {
+    e.preventDefault();
+    var submenu = $(this).next(".submenu");
+    var submenuId = submenu.attr("id");
+
+    if (submenu.is(":visible")) {
+      submenu.hide();
+      saveSubmenuState(submenuId, false); // Guardar el estado del submenú como cerrado
+    } else {
+      submenu.show();
+      saveSubmenuState(submenuId, true); // Guardar el estado del submenú como abierto
+    }
+  });
+});
 
